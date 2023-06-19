6dbe265c9b6205aaabbb8d9da6f4c1c38b26cb9d
Ахмедов Руфат Фаиг <Ahmed_ur54@itstep.edu.az>
2023-06-19 18:46:18 +0400

    Library

---
+++ JsonRepository.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibProject;
+
+public class JsonRepository
+{
+    const string path = "books.json";
+    public List<Library> LoadBooks()
+    {
+        List<Library> books = new List<Library>();
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            books = JsonConvert.DeserializeObject<List<Library>>(json);
+        }
+
+        return books;
+    }
+    public int GetLastBookId(List<Library> books)
+    {
+        if (books.Count > 0)
+            return books[books.Count - 1].Id;
+        return 0;
+    }
+    public void SaveBooks(List<Library> books)
+    {
+        string json = JsonConvert.SerializeObject(books, Newtonsoft.Json.Formatting.Indented);
+        File.WriteAllText(path, json);
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace LibProject;
+
+
+public partial class MainWindow : Window
+{
+    private JsonRepository repository = new JsonRepository();
+    const string path = "books.json";
+    public MainWindow()
+    {
+        InitializeComponent();
+        BooksListView.ItemsSource = repository.LoadBooks();
+    }
+
+    private void SetDefault()
+    {
+        ShowMenu.Visibility = Visibility.Hidden;
+        AddMenu.Visibility = Visibility.Hidden;
+        EditMenu.Visibility = Visibility.Hidden;
+        DeleteMenu.Visibility = Visibility.Hidden;
+    }
+
+    private void ShowButton_Click(object sender, RoutedEventArgs e)
+    {
+        SetDefault();
+        ShowMenu.Visibility = Visibility.Visible;
+    }
+
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        SetDefault();
+        AddMenu.Visibility = Visibility.Visible;
+    }
+
+    private void EditButton_Click(object sender, RoutedEventArgs e)
+    {
+        SetDefault();
+        EditMenu.Visibility = Visibility.Visible;
+    }
+
+    private void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+        SetDefault();
+        DeleteMenu.Visibility = Visibility.Visible;
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e) // Add
+    {
+        try
+        {
+            List<Library> books = repository.LoadBooks();
+
+            Library newBook = new Library
+            {
+                Id = repository.GetLastBookId(books) + 1,
+                Title = TitleTextBox.Text,
+                Author = AuthorTextBox.Text,
+                PublicationYear = int.Parse(PublicationYearTextBox.Text),
+            };
+            books.Add(newBook);
+
+            repository.SaveBooks(books);
+            BooksListView.ItemsSource = repository.LoadBooks();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(
+               messageBoxText: ex.Message,
+               caption: "Publication Year Uncorrect",
+               button: MessageBoxButton.OK,
+               icon: MessageBoxImage.Error);
+        }
+    }
+
+    private void Button_Click_2(object sender, RoutedEventArgs e) // Edit
+    {
+
+        try
+        {
+            List<Library> books = repository.LoadBooks();
+
+            if (int.Parse(editBookId.Text) > repository.GetLastBookId(books))
+                throw new IndexOutOfRangeException("There is no such id");
+
+            foreach (var book in books)
+            {
+                if (book.Id == int.Parse(editBookId.Text))
+                {
+                    book.Title = newTitleTextBox.Text;
+                    book.Author = newAuthorTextBox.Text;
+                    book.PublicationYear = int.Parse(newPublicationYearTextbox.Text);
+                }
+            }
+
+            repository.SaveBooks(books);
+            BooksListView.ItemsSource = repository.LoadBooks();
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            MessageBox.Show(
+                messageBoxText: ex.Message,
+                caption: "Error",
+                button: MessageBoxButton.OK,
+                icon: MessageBoxImage.Error);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(
+               messageBoxText: "Id Or Publication Year Uncorrect",
+               caption: "Error",
+               button: MessageBoxButton.OK,
+               icon: MessageBoxImage.Error);
+        }
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e) // Delete
+    {
+        try
+        {
+            List<Library> books = repository.LoadBooks();
+            if (int.Parse(deleteBookId.Text) > repository.GetLastBookId(books))
+                throw new IndexOutOfRangeException("There is no such id");
+            books.RemoveAt(int.Parse(deleteBookId.Text) - 1);
+            repository.SaveBooks(books);
+            BooksListView.ItemsSource = repository.LoadBooks();
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            MessageBox.Show(
+                messageBoxText: ex.Message,
+                caption: "Error",
+                button: MessageBoxButton.OK,
+                icon: MessageBoxImage.Error);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(
+               messageBoxText: "Id data type uncorrect",
+               caption: "Error",
+               button: MessageBoxButton.OK,
+               icon: MessageBoxImage.Error);
+        }
+    }
+
+    private void ExitButton_Click(object sender, RoutedEventArgs e) => this.Close();
+}
